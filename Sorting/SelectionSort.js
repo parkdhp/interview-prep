9d51ff05b9242aaa60e668cf233dcdf70be35035
Peter Park <parkdhp@gmail.com>
2022-03-18 18:46:54 -0700

    feat(sorting): add selection sort

---
+++ Sorting/SelectionSort.js
@@ -0,0 +1,22 @@
+const selectionSort = (arr) => {
+  let n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    // Finding the smallest number in the subarray
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      // Swapping the elements
+      let tmp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = tmp;
+    }
+  }
+  return arr;
+};
+
+console.log(selectionSort([4, 3, 6, 1, 2]));
