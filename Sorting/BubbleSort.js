cdd25f1abfd41f8b92d46a36b7702354107a411b
Peter Park <parkdhp@gmail.com>
2022-03-20 10:38:40 -0700

    feat(sorting): add bubble sort

---
+++ Sorting/BubbleSort.js
@@ -0,0 +1,11 @@
+const bubbleSort = (arr) => {
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
