0469b66b950c553b7422b7292e6b5e62f250adad
Peter Park <parkdhp@gmail.com>
2022-07-19 23:07:55 -0700

    feat(arrays): add array quadruplet

---
+++ Arrays/ArrayQuadruplet.js
@@ -0,0 +1,50 @@
+/*
+ * Array Quadruplet
+ * Given an unsorted array of integers arr and a number s, write a function findArrayQuadruplet that finds four numbers (quadruplet) in arr that sum up to s. Your function should return an array of these numbers in an ascending order. If such a quadruplet doesn’t exist, return an empty array.
+ * Note that there may be more than one quadruplet in arr whose sum is s. You’re asked to return the first one you encounter (considering the results are sorted).
+ * Explain and code the most efficient solution possible, and analyze its time and space complexities.
+ *
+ * Example:
+ * input:  arr = [2, 7, 4, 0, 9, 5, 1, 3], s = 20
+ * output: [0, 4, 7, 9] # The ordered quadruplet of (7, 4, 0, 9)
+ *                      # whose sum is 20. Notice that there
+ *                      # are two other quadruplets whose sum is 20:
+ *                      # (7, 9, 1, 3) and (2, 4, 9, 5), but again you’re
+ *                      # asked to return the just one quadruplet (in an
+ *                      # ascending order)
+ * Constraints:
+ * [time limit] 5000ms
+ * [input] array.integer arr
+ * 1 ≤ arr.length ≤ 100
+ * [input] integer s
+ * [output] array.integer
+ */
+
+const findArrayQuadruplet = (arr, s) => {
+  const n = arr.length;
+  if (n < 4) {
+    return [];
+  }
+  const len = arr.length;
+  arr = arr.sort((a, b) => a - b);
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      let left = j + 1;
+      let right = len - 1;
+      const findSum = s - (arr[i] + arr[j]);
+      while(left < right) {
+        if(arr[left] + arr[right] > findSum){
+          right--;
+        } else if(arr[left] + arr[right] < findSum) {
+          left++;
+        } else {
+          return [arr[i], arr[j], arr[left], arr[right]];
+        }
+      }
+    }
+  }
+  return [];
+};
+// Time: O(n^3)
+// Space: O(1)
+console.log(findArrayQuadruplet([2, 7, 4, 0, 9, 5, 1, 3], 20));
